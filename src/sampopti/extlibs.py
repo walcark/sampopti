3439f57129439ee4cdeb8db40fe0fc55e3fdac7a
Kévin Walcarius <kevin.walcarius@gmail.com>
2025-06-03 14:34:33 +0200

    Adding Smart-G related imports.

---
+++ src/sampopti/extlibs.py
@@ -0,0 +1,30 @@
+############################################################################
+# SmartG imports                                                           #
+############################################################################
+import smartg.visualizegeo as sg_vis
+Plane = sg_vis.Plane
+Transformation = sg_vis.Transformation
+Entity = sg_vis.Entity
+Matte = sg_vis.Matte
+
+import smartg.smartg as sg
+LambSurface = sg.LambSurface
+Albedo_cst = sg.Albedo_cst
+Environment = sg.Environment
+Smartg = sg.Smartg
+Sensor = sg.Sensor
+
+import smartg.atmosphere as sg_atm
+AtmAFGL = sg_atm.AtmAFGL
+AerOPAC = sg_atm.AerOPAC
+
+import smartg.geometry as sg_geo
+Point = sg_geo.Point
+
+############################################################################
+# LUT imports                                                              #
+############################################################################
+import luts.luts as luts
+MLUT = luts.MLUT
+LUT = luts.LUT
+
